c93e3ae2cc13f0a009989c1fa11db79aa47ef9ce
milan-d-simform <124656254+milan-d-simform@users.noreply.github.com>
2024-06-26 11:00:51 +0530

    clear canvas (#14)
    * avtar issue solved
    
    * File renamed
    
    * File renamed
    
    * File name renamed
    
    * Shared canvas
    
    ---------
    
    Co-authored-by: Milan Dadhaniya <simform.milan.dadhaniya@carsaver.com>

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,13 @@
 import { useConnectionStore } from "@store/connection";
 import { useUsersStore } from "@store/user";
 import { useMessageStore } from '@store/message';
+import { useDrawingStore } from '@store/drawing';
 import { socket } from "@src/socket";
 
 const connectionStore = useConnectionStore();
 const usersStore = useUsersStore();
 const messageStore = useMessageStore();
+const drawingStore = useDrawingStore();
 
 // remove any existing listeners (after a hot module replacement)
 socket.off();
@@ -14,6 +16,7 @@ socket.off();
 connectionStore.bindEvents();
 usersStore.bindEvents();
 messageStore.bindEvents();
+drawingStore.bindEvents();
 </script>
 
 <template>
