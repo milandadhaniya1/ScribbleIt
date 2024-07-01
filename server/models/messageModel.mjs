3b67e6679525db850e18cce00b33b6adcc72463b
Milan Dadhaniya <simform.milan.dadhaniya@carsaver.com>
2024-07-01 11:19:55 +0530

    Added database integration

--- server/models/messageModel.mjs
+++ server/models/messageModel.mjs
@@ -4,9 +4,11 @@ const messageSchema = new mongoose.Schema({
   userId: String,
   username: String,
   message: String,
+  messageTime: String,
+  type: String,
   timestamp: { type: Date, default: Date.now }
 });
 
-const Message = mongoose.model('Message', messageSchema);
+const Message = mongoose.model('message', messageSchema);
 
 export default Message;
